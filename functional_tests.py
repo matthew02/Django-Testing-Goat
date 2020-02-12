1ed64d7ed7afdf2ab37979b480c636188cfd1606
James Smith <matthew02@users.noreply.github.com>
2020-02-12 13:49:12 -0600

    Added environment variable for browser path

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,12 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 
-BINARY = FirefoxBinary('/usr/bin/firefox-developer-edition')
+FIREFOX_PATH = os.environ.get('FIREFOX_PATH')
+print(FIREFOX_PATH)
+BINARY = FirefoxBinary(FIREFOX_PATH)
 BROWSER = webdriver.Firefox(firefox_binary=BINARY)
 #BROWSER = webdriver.Firefox()
 BROWSER.get('http://localhost:8000')
